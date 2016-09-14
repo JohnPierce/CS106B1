27b03363af2c257d8a58a8f7bfd351dded207202
John Pierce <jjvppierce@sbcglobal.net>
2016-09-13 22:06:52 -0700

    Initial Commit

---
+++ CS106B-SectionHandout1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS106B-SectionHandout1
+//
+//  Created by John Pierce on 9/13/16.
+//  Copyright © 2016 JohnPierce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
